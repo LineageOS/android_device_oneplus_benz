457ba081c5ed6d9591b900758d4377ddd11846d3
inferno0230 <mail@inferno0230.in>
2025-08-17 12:32:24 +0530

    benz: init: Set region for OnePlus Nord CE4
    Change-Id: I9cdfc85fb8097e30a3f3d2aab66fa048319ae846
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -204,8 +204,6 @@ PRODUCT_PACKAGES += \
     ueventd.oplus.rc \
     ueventd.qcom.rc
 
-$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_oplus)
-
 # Kernel
 PRODUCT_COPY_FILES += \
     kernel/oneplus/sm8550/modules.systemdlkm_blocklist.msm.kalama:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
